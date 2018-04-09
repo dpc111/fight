3f7a92115436aac148889bdc903868db5dcc9faf
dpc111 <1539309743@qq.com>
2018-04-09 19:06:53 +0800

    l

---
+++ fight/Assets/Scripts/Grid/GridInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridInfo : MonoBehaviour {
+    public bool Used = false;
+    public Vector3 Center = new Vector3(0, 0, 0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ fight/Assets/Scripts/Grid/GridMgr.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMgr : MonoBehaviour {
+    public Dictionary<int, GridInfo> Grids = new Dictionary<int, GridInfo>();
+    public Material UnUsedMat;
+    public Material UsedMat;
+    public bool IsVisible = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeVisible(bool visible)
+    {
+        if (IsVisible == visible)
+        {
+            return;
+        }
+        IsVisible = visible;
+        foreach (Transform tran in transform)
+        {
+            tran.GetComponent<Renderer>().enabled = IsVisible;
+        }
+    }
+
+    public void SetGridUsed(Transform grid)
+    {
+        grid.GetComponent<Renderer>().material = UsedMat;
+    }
+
+
+    public void SetGridUnUsed(Transform grid)
+    {
+        grid.GetComponent<Renderer>().material = UnUsedMat;
+    }
+
+    public int GridId(int row, int col)
+    {
+        return row * 100 + col;
+    }
+}
