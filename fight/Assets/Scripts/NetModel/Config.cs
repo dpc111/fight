5eff63cbd8ce47d761d15e9c48f508e0f7ea3b71
dpc111 <1539309743@qq.com>
2018-04-26 19:10:26 +0800

    l

---
+++ fight/Assets/Scripts/NetModel/Config.cs
@@ -0,0 +1,16 @@
+﻿namespace NetModel
+{
+    using System;
+    using System.Net.Sockets;
+    using System.Net;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Runtime.Remoting.Messaging;
+
+    public class Config
+    {
+        public static int tcpPacketMax = 1024;
+    }
+}
---
+++ fight/Assets/Scripts/NetModel/PacketSender.cs
@@ -0,0 +1,21 @@
+﻿namespace NetModel
+{
+    using System;
+    using System.Net.Sockets;
+    using System.Net;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Text.RegularExpressions;
+    using System.Runtime.Remoting.Messaging;
+
+    public class PacketSender
+    {
+        public delegate void AsyncSenderMethod();
+        public PacketSender(Network) 
+        {
+
+        }
+        
+    }
+}
