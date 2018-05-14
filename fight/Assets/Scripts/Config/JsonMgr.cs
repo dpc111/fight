228ca3ba6771f461dea608f49e4b134fb0c3b2f3
dpc111 <1539309743@qq.com>
2018-05-14 19:19:49 +0800

    l

---
+++ fight/Assets/Scripts/Config/JsonMgr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class ConfigMgr : MonoBehaviour {
+
+    public static Dictionary<string, JObject> jsonObjects = new Dictionary<string, JObject>();
+
+	void Start () {
+		
+	}
+
+    void Load(string fileName, string jsonName)
+    {
+        JObject obj = null;
+        if (!jsonObjects.TryGetValue(jsonName, out obj))
+        {
+            return;
+        }
+        string fileData = File.ReadAllText(fileName, Encoding.UTF8);
+        string jsonData = "{" + jsonName + ":" + fileData + "}";
+        JObject jsonObj = JObject.Parse(jsonData);
+        jsonObjects[jsonName] = jsonObj;
+    }
+
+    static JToken GetValue(string jsonName, object key1, object key2)
+    {
+        JObject obj = null;
+        if (!jsonObjects.TryGetValue(jsonName, out obj))
+        {
+            return 0;
+        }
+        return obj[jsonName][key1][key2];
+    }
+}
---
+++ fight/Assets/Scripts/Test/JsonTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class JsonTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        string json = " { battle : " + File.ReadAllText("D:/self/game1/server/etc/config/json/battle.json", Encoding.UTF8) + "}";
+        Debug.Log(json);
+        JObject obj = JObject.Parse(json);
+        Debug.Log(obj["battle"][3]["roomName"].ToString());
+        //Debug.Log(obj[4]["id"].ToString());
+
+        JArray ints = (JArray)obj["battle"];
+
+        foreach (var inter in ints)
+        {
+            Debug.Log(inter["id"]);
+        }
+    }
+
+}
