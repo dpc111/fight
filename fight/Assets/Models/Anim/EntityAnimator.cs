e88f9ccd3c39be98f7ac5f75b105f7ff6afeffaf
dpc111 <1539309743@qq.com>
2018-07-25 18:20:03 +0800

    l

---
+++ fight/Assets/Models/Anim/EntityAnimator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityAnimator : MonoBehaviour {
+    Animator animator;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetInteger("state", 1);
+            Debug.Log("1");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetInteger("state", 2);
+            Debug.Log("2");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetInteger("state", 3);
+            Debug.Log("3");
+        }
+        if (!animator.IsInTransition(0))
+        {
+            Debug.LogError("");
+            int state = animator.GetInteger("state");
+            if (stateInfo.fullPathHash == Animator.StringToHash("Base Layer.Idle"))
+            {
+                
+            }
+            if (stateInfo.fullPathHash == Animator.StringToHash("Base Layer.Attack"))
+            {
+                //animator.SetInteger("state", 1);
+            }
+            if (stateInfo.fullPathHash == Animator.StringToHash("Base Layer.Death"))
+            {
+                //animator.SetInteger("state", 1);
+            }
+        }
+	}
+}
